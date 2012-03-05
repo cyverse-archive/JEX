802ced05a5d96b9c8bf746d091144e5d32f3d2bd
John Wregglesworth <wregglej@iplantcollaborative.org>
2012-03-05 10:06:05 -0700

    CORE-2952: Modified the JEX to return an error map regardless of where the error is raised.

--- project.clj
+++ project.clj
@@ -7,6 +7,7 @@
                  [compojure "1.0.1"]
                  [ring/ring-jetty-adapter "1.0.1"]
                  [clj-http "0.2.5"]
-                 [org.iplantc/clojure-commons "1.1.0-SNAPSHOT"]]
+                 [org.iplantc/clojure-commons "1.1.0-SNAPSHOT"]
+                 [slingshot "0.10.1"]]
   :aot [jex.core]
   :main jex.core)
