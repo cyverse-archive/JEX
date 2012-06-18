4071cca52d1684a7327722510169ddf9f1907bd7
John Wregglesworth <wregglej@iplantcollaborative.org>
2012-06-18 09:12:07 -0700

    Migrated the JEX over to leiningen 2.

--- project.clj
+++ project.clj
@@ -1,5 +1,4 @@
-(defproject jex "1.0.0-SNAPSHOT"
-  :description "FIXME: write"
+(defproject jex/jex "1.0.0-SNAPSHOT" 
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/data.json "0.1.1"]
                  [org.clojure/tools.logging "0.2.3"]
@@ -10,4 +9,6 @@
                  [org.iplantc/clojure-commons "1.1.0-SNAPSHOT"]
                  [slingshot "0.10.1"]]
   :aot [jex.core]
-  :main jex.core)
+  :main jex.core
+  :min-lein-version "2.0.0"
+  :description "FIXME: write")
