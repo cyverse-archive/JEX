0100a8794284078b852be3651bec624b0874be0a
John Wregglesworth <wregglej@iplantcollaborative.org>
2012-06-18 10:27:29 -0700

    Changed version to a -SNAPSHOT.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject jex/jex "0.2.0" 
+(defproject jex/jex "0.2.0-SNAPSHOT" 
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/data.json "0.1.1"]
                  [org.clojure/tools.logging "0.2.3"]
