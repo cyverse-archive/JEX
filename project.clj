2d394aba77bd3c24d6bdcb999068cb0a0b5e9649
John Wregglesworth <wregglej@iplantcollaborative.org>
2012-06-18 09:19:53 -0700

    Switch the JEX over to using iPlant's lein plugin for creating backend RPMs.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject jex/jex "1.0.0-SNAPSHOT" 
+(defproject jex/jex "0.2.0" 
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/data.json "0.1.1"]
                  [org.clojure/tools.logging "0.2.3"]
@@ -8,7 +8,17 @@
                  [clj-http "0.2.5"]
                  [org.iplantc/clojure-commons "1.1.0-SNAPSHOT"]
                  [slingshot "0.10.1"]]
+  :iplant-rpm {:summary "jex",
+               :release 1,
+               :dependencies ["iplant-service-config >= 0.1.0-5"],
+               :config-files ["log4j.properties"],
+               :config-path "conf"}
   :aot [jex.core]
   :main jex.core
   :min-lein-version "2.0.0"
-  :description "FIXME: write")
+  :repositories {"iplantCollaborative"
+                 "http://projects.iplantcollaborative.org/archiva/repository/internal/",
+                 "renci.repository"
+                 "http://ci-dev.renci.org/nexus/content/repositories/snapshots/"}
+  :plugins [[org.iplantc/lein-iplant-rpm "1.2.1-SNAPSHOT"]]
+  :description "A backend job execution service that submits jobs to Condor.")
