8d7d6c9ce0bc252f8b198d6ae6ca82454fa74634
John Wregglesworth <wregglej@iplantcollaborative.org>
2012-08-27 14:56:40 -0700

    Added a test for the jex.dagify/script function.

--- test/jex/test/dagify.clj
+++ test/jex/test/dagify.clj
@@ -55,3 +55,78 @@
     "if [ ! \"$?\" -eq \"0\" ]; then\n"
     "\tEXITSTATUS=1\n"
     "fi\n"))
+
+(def test-analysis
+  {:imkdir-job
+   {:environment "imkdir-env"
+    :executable "imkdir-exec"
+    :arguments "imkdir-args"
+    :stderr "imkdir-stderr"
+    :stdout "imkdir-stdout"}
+   :final-output-job
+   {:environment "final-job-env"
+    :executable "final-job-exec"
+    :arguments "final-job-args"
+    :stderr "final-job-stderr"
+    :stdout "final-job-stdout"}
+   :all-input-jobs
+   [{:environment "input-1-env"
+     :executable "input-1-exec"
+     :arguments "input-1-args"
+     :stderr "input-1-stderr"
+     :stdout "input-1-stdout"}
+    {:environment "input-2-env"
+     :executable "input-2-exec"
+     :arguments "input-2-args"
+     :stderr "input-2-stderr"
+     :stdout "input-2-stdout"}]
+   :steps
+   [{:environment "step-1-env"
+     :executable "step-1-exec"
+     :arguments "step-1-args"
+     :stderr "step-1-stderr"
+     :stdout "step-1-stdout"}
+    {:environment "step-2-env"
+     :executable "step-2-exec"
+     :arguments "step-2-args"
+     :stderr "step-2-stderr"
+     :stdout "step-2-stdout"}]
+   :uuid "testuuid"
+   :username "testuser"})
+
+(fact
+ (script test-analysis) =>
+ (str
+  "#!/bin/bash\n"
+  "cd ~\n"
+  "mkdir -p iplant-de-jobs/testuser/testuuid\n"
+  "pushd iplant-de-jobs/testuser/testuuid\n"
+  "mkdir -p logs\n"
+  "EXITSTATUS=0\n"
+  "imkdir-env imkdir-exec imkdir-args 1> imkdir-stdout 2> imkdir-stderr\n"
+  "if [ ! \"$?\" -eq \"0\" ]; then\n"
+  "\tEXITSTATUS=1\n"
+  "fi\n\n"
+  "input-1-env input-1-exec input-1-args 1> input-1-stdout 2> input-1-stderr\n"
+  "if [ ! \"$?\" -eq \"0\" ]; then\n"
+  "\tEXITSTATUS=1\n"
+  "fi\n\n"
+  "input-2-env input-2-exec input-2-args 1> input-2-stdout 2> input-2-stderr\n"
+  "if [ ! \"$?\" -eq \"0\" ]; then\n"
+  "\tEXITSTATUS=1\n"
+  "fi\n\n"
+  "step-1-env step-1-exec step-1-args 1> step-1-stdout 2> step-1-stderr\n"
+  "if [ ! \"$?\" -eq \"0\" ]; then\n"
+  "\tEXITSTATUS=1\n"
+  "fi\n\n"
+  "step-2-env step-2-exec step-2-args 1> step-2-stdout 2> step-2-stderr\n"
+  "if [ ! \"$?\" -eq \"0\" ]; then\n"
+  "\tEXITSTATUS=1\n"
+  "fi\n\n"
+  "final-job-env final-job-exec final-job-args 1> final-job-stdout 2> final-job-stderr\n"
+  "if [ ! \"$?\" -eq \"0\" ]; then\n"
+  "\tEXITSTATUS=1\n"
+  "fi\n"
+  "popd\n"
+  "rm -r iplant-de-jobs/testuser/testuuid\n"
+  "exit $EXITSTATUS\n"))
