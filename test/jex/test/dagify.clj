2b0658a1a89e16a8abbbaa8edcc3ccbe6545bfe9
John Wregglesworth <wregglej@iplantcollaborative.org>
2012-08-28 09:12:08 -0700

    Added more unit tests that have side-effects. This time it's generate-script-submission.

--- test/jex/test/dagify.clj
+++ test/jex/test/dagify.clj
@@ -176,4 +176,99 @@
 
  (.exists (java.io.File. "test/scratch/condor-log")) => true?)
 (.delete (java.io.File. "test/scratch/condor-log"))
+
+
+
+(def test-gen-map
+  {:working_dir "test/scratch/"
+   :condor-log-dir "test/scratch/"
+   :username "testuser"
+   :uuid "testuuid"
+   :imkdir-job
+   {:environment "imkdir-env"
+    :executable "imkdir-exec"
+    :arguments "imkdir-args"
+    :stderr "imkdir-stderr"
+    :stdout "imkdir-stdout"}
+   :final-output-job
+   {:environment "final-job-env"
+    :executable "final-job-exec"
+    :arguments "final-job-args"
+    :stderr "final-job-stderr"
+    :stdout "final-job-stdout"}
+   :all-input-jobs
+   [{:environment "input-1-env"
+     :executable "input-1-exec"
+     :arguments "input-1-args"
+     :stderr "input-1-stderr"
+     :stdout "input-1-stdout"}
+    {:environment "input-2-env"
+     :executable "input-2-exec"
+     :arguments "input-2-args"
+     :stderr "input-2-stderr"
+     :stdout "input-2-stdout"}]
+   :steps
+   [{:environment "step-1-env"
+     :executable "step-1-exec"
+     :arguments "step-1-args"
+     :stderr "step-1-stderr"
+     :stdout "step-1-stdout"}
+    {:environment "step-2-env"
+     :executable "step-2-exec"
+     :arguments "step-2-args"
+     :stderr "step-2-stderr"
+     :stdout "step-2-stdout"}]})
+
+(.mkdirs (java.io.File. (str (:working_dir test-gen-map) "logs")))
+(.mkdirs (java.io.File. (:condor-log-dir test-gen-map)))
+
+(def result-gen-script
+  (generate-script-submission test-gen-map))
+
+(fact
+ (.exists (java.io.File. "test/scratch/logs/iplant.cmd")) => true?
+ (.exists (java.io.File. "test/scratch/logs/iplant.sh")) => true?
+ 
+ (slurp "test/scratch/logs/iplant.cmd") =>
+ "universe = vanilla\nexecutable = /bin/bash\narguments = \"test/scratch/logs/iplant.sh\"\noutput = test/scratch/logs/script-output.log\nerror = test/scratch/logs/script-error.log\nlog = test/scratch/logs/script-condor-log\n+IpcUuid = \"testuuid\"\n+IpcJobId = \"generated_script\"\n+IpcUsername = \"testuser\"\nshould_transfer_files = NO\nnotification = NEVER\nqueue\n"
+
+ (slurp "test/scratch/logs/iplant.sh") =>
+ (str
+  "#!/bin/bash\n"
+  "cd ~\n"
+  "mkdir -p iplant-de-jobs/testuser/testuuid\n"
+  "pushd iplant-de-jobs/testuser/testuuid\n"
+  "mkdir -p logs\n"
+  "EXITSTATUS=0\n"
+  "imkdir-env imkdir-exec imkdir-args 1> imkdir-stdout 2> imkdir-stderr\n"
+  "if [ ! \"$?\" -eq \"0\" ]; then\n"
+  "\tEXITSTATUS=1\n"
+  "fi\n\n"
+  "input-1-env input-1-exec input-1-args 1> input-1-stdout 2> input-1-stderr\n"
+  "if [ ! \"$?\" -eq \"0\" ]; then\n"
+  "\tEXITSTATUS=1\n"
+  "fi\n\n"
+  "input-2-env input-2-exec input-2-args 1> input-2-stdout 2> input-2-stderr\n"
+  "if [ ! \"$?\" -eq \"0\" ]; then\n"
+  "\tEXITSTATUS=1\n"
+  "fi\n\n"
+  "step-1-env step-1-exec step-1-args 1> step-1-stdout 2> step-1-stderr\n"
+  "if [ ! \"$?\" -eq \"0\" ]; then\n"
+  "\tEXITSTATUS=1\n"
+  "fi\n\n"
+  "step-2-env step-2-exec step-2-args 1> step-2-stdout 2> step-2-stderr\n"
+  "if [ ! \"$?\" -eq \"0\" ]; then\n"
+  "\tEXITSTATUS=1\n"
+  "fi\n\n"
+  "final-job-env final-job-exec final-job-args 1> final-job-stdout 2> final-job-stderr\n"
+  "if [ ! \"$?\" -eq \"0\" ]; then\n"
+  "\tEXITSTATUS=1\n"
+  "fi\n"
+  "popd\n"
+  "rm -r iplant-de-jobs/testuser/testuuid\n"
+  "exit $EXITSTATUS\n"))
+
+(.delete (java.io.File. "test/scratch/logs/iplant.cmd"))
+(.delete (java.io.File. "test/scratch/logs/iplant.sh"))
+(.delete (java.io.File. "test/scratch/logs"))
 (.delete (java.io.File. "test/scratch"))
