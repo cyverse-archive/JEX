08cd6d7566b9f254f92e8bcc3cf4dea913b021dc
John Wregglesworth <wregglej@iplantcollaborative.org>
2013-04-16 10:46:45 -0700

    Cleaned up outgoing_xforms.clj.

--- src/jex/outgoing_xforms.clj
+++ src/jex/outgoing_xforms.clj
@@ -2,21 +2,21 @@
 
 (defn filter-map
   [outgoing-map]
-  (let [username (:username outgoing-map)]
-    (-> outgoing-map
-    (assoc :status "Submitted")
-    (assoc :user username)
-    (assoc :output_manifest [])
-    (dissoc :username)
-    (dissoc :dag)
-    (dissoc :final-output-job)
-    (dissoc :steps)
-    (dissoc :all-input-jobs :all-output-jobs :imkdir-job))))
+  (-> outgoing-map
+    (assoc :status          "Submitted"
+           :user            (:username outgoing-map)
+           :output_manifest [])
+    (dissoc :username
+            :dag
+            :final-output-job
+            :steps
+            :all-input-jobs
+            :all-output-jobs
+            :imkdir-job)))
 
 (defn transform
   "Applies some transformations to the condor-map. Basically used
    to clean stuff up before dumping information into the OSM."
   [outgoing-map]
-  (-> outgoing-map
-    filter-map))
+  (filter-map outgoing-map))
 
