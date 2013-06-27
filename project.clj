05d2eda2715b35859edd08d1e86c410508303c00
John Wregglesworth <wregglej@iplantcollaborative.org>
2013-06-27 10:20:58 -0700

    CORE-4288: Added iplant-clavin as a dependency.

--- project.clj
+++ project.clj
@@ -9,7 +9,7 @@
                  [slingshot "0.10.3"]]
   :iplant-rpm {:summary "jex",
                :runuser "condor"
-               :dependencies ["iplant-service-config >= 0.1.0-5"],
+               :dependencies ["iplant-service-config >= 0.1.0-5" "iplant-clavin"],
                :config-files ["log4j.properties"],
                :config-path "conf"}
   :profiles {:dev {:dependencies [[midje "1.4.0"]
